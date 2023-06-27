6df478b2be33c6e87ec29e219a5727e62d448846
minibxx <alsdbqls10@duksung.ac.kr>
2023-06-28 01:14:13 +0900

    리셋 테이블 수정

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,9 @@
 jQuery(document).ready(function ($) {
     $('body').prepend('<header></header>');
-    //$('body').append('<footer></footer>');
+    $('body').append('<footer></footer>');
     $('header').load('./header.html nav');
-    
+    $('footer').load('./footer.html .ft');
+
     $(".counter1 code").counterUp({
         delay: 10,
         time: 1000,
