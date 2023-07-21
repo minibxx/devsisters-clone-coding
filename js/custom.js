73821ee157cbffbcd9077e64d185c162874314d5
6bjs4112 <101965472+minibxx@users.noreply.github.com>
2023-07-21 18:19:41 +0900

    메인 수정푸터 로고 이미지랑 슬라이더만 손보면 됨

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
     $('body').prepend('<header></header>');
     $('body').append('<footer></footer>');
     $('header').load('./header.html');
-    $('footer').load('./footer.html .ft');
+    $('footer').load('./footer.html');
 
 });
 
